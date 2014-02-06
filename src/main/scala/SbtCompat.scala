2bfcdcc5ea65bd2ee09ad724fb6f059c91e232cc
Eugene Yokota <eed3si9n@gmail.com>
2014-02-05 23:23:43 -0500

    hardcode to sbt 0.13

--- src/main/scala/SbtCompat.scala
+++ src/main/scala/SbtCompat.scala
@@ -1,6 +1,8 @@
 package sbtappengine
 
 import sbt._
+import Keys._
+import spray.revolver.RevolverKeys
 
 /**
  * Sbt compatibility code concrete instances are declared in sbt version specific folders
@@ -16,4 +18,15 @@ trait SbtCompat {
 
 object SbtCompat {
   def impl: SbtCompat = SbtCompatImpl
+}
+
+// ----
+
+object SbtCompatImpl extends SbtCompat with RevolverKeys {
+  import Plugin.{AppengineKeys => gae}
+
+  def changeJavaOptions(f: (File, File, String, File, Boolean, Int) => Seq[String]) =
+    javaOptions in gae.devServer <<= (gae.overridesJarPath, gae.agentJarPath, gae.reJRebelJar,
+      gae.localDbPath in gae.devServer,
+      gae.debug in gae.devServer, gae.debugPort in gae.devServer) map f
 }
